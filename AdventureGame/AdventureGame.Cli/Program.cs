6e67e049f7365f6659f703b13bf2faeacf0d7829
STIDHAMRC <STIDHAMRC@MLC12036>
2026-01-28 16:56:49 -0500

    First Day

---
+++ AdventureGame/AdventureGame.Cli/Program.cs
@@ -0,0 +1,11 @@
+﻿using AdventureGame.Core;
+namespace AdventureGame.Cli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
